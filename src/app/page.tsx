66ee50ed26bb494502ba926273dc73de83bce7ad
Nikita Plyako <nikito4ka281205@gmail.com>
2024-10-21 01:14:36 +0300

    create reviews section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 export default function Home() {
     return (
-        <div className="bg-slate-50">
-            <section>
+        <div>
+            <section className="bg-slate-50">
                 <Container>
                     <div className="flex justify-between flex-col sm:gap-10 items-center py-16 sm:py-24 lg:flex-row lg:gap-0">
                         <div className="relative max-w-full lg:max-w-[540px] xl:max-w-[700px]">
@@ -28,8 +28,8 @@ export default function Home() {
                                 <span className="font-semibold">
                                     one-of-one
                                 </span>{' '}
-                                phone case. CatPillow allows you to protect your
-                                memories, not just your phone case.
+                                pillow. CatPillow allows you to cuddle with your
+                                memories, not just display them.
                             </p>
 
                             <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center lg:items-start">
@@ -43,7 +43,7 @@ export default function Home() {
                                 </li>
                                 <li className="flex gap-1.5 items-center">
                                     <Check className="h-5 w-5 text-purple-600" />
-                                    Beautiful pillows supported
+                                    Beautifully crafted pillows
                                 </li>
                             </ul>
 
@@ -127,6 +127,136 @@ export default function Home() {
                                 src="/your-image.png"
                                 alt="User"
                             />
+                        </div>
+                    </div>
+                </Container>
+            </section>
+
+            {/* what customers say */}
+            <section className="bg-slate-100 py-16 md:py-24">
+                <Container>
+                    <div className="flex flex-col lg:flex-row items-center justify-center gap-0 lg:gap-4">
+                        <h2 className="order-1 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                            What our{' '}
+                            <span className="relative">
+                                customers{' '}
+                                <svg
+                                    className="absolute hidden sm:block -bottom-6 left-0 text-purple-500 inset-x-0 pointer-events-none"
+                                    viewBox="0 0 687 155">
+                                    <g
+                                        stroke="currentColor"
+                                        strokeWidth="7"
+                                        fill="none"
+                                        fillRule="evenodd"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round">
+                                        <path
+                                            d="M20 98c27-13.3333333 54-20 81-20 40.5 0 40.5 20 81 20s40.626917-20 81-20 40.123083 20 80.5 20 40.5-20 81-20 40.5 20 81 20 40.626917-20 81-20c26.915389 0 53.748722 6.6666667 80.5 20"
+                                            opacity=".3"></path>
+                                        <path d="M20 118c27-13.3333333 54-20 81-20 40.5 0 40.5 20 81 20s40.626917-20 81-20 40.123083 20 80.5 20 40.5-20 81-20 40.5 20 81 20 40.626917-20 81-20c26.915389 0 53.748722 6.6666667 80.5 20"></path>
+                                    </g>
+                                </svg>
+                            </span>{' '}
+                            say
+                        </h2>
+                        <Image
+                            width={150}
+                            height={50}
+                            src="/stickers/panda-lies.png"
+                            alt="panda"
+                            className="order-0 lg:order-2"
+                        />
+                    </div>
+
+                    <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-y-16 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                        <div className="flex flex-col gap-4 lg:pr-16">
+                            <div className="flex gap-0.5 mb-2">
+                                <Star className="h-5 w-5 text-purple-600 fill-purple-600" />
+                                <Star className="h-5 w-5 text-purple-600 fill-purple-600" />
+                                <Star className="h-5 w-5 text-purple-600 fill-purple-600" />
+                                <Star className="h-5 w-5 text-purple-600 fill-purple-600" />
+                                <Star className="h-5 w-5 text-purple-600 fill-purple-600" />
+                            </div>
+
+                            <div className="text-lg leading-8">
+                                <p>
+                                    {'"'}The pillow feels incredibly soft and I
+                                    even got a compliment on the design. I{"'"}
+                                    ve had the pillow for two and a half months
+                                    now, and the{' '}
+                                    <span className="px-0.5 bg-slate-700 text-white">
+                                        image is still super clear
+                                    </span>
+                                    . On the pillow I had before, the design
+                                    started fading after just a few weeks. Love
+                                    it!{'"'}
+                                </p>
+                            </div>
+
+                            <div className="flex gap-4 mt-2 items-center">
+                                <Image
+                                    width={50}
+                                    height={50}
+                                    className="rounded-full h-10 w-10 object-cover ring-slate-100"
+                                    src="/users/user-1.jpg"
+                                    alt="User"
+                                />
+                                <div>
+                                    <p className="font-semibold">Anna</p>
+                                    <div className="flex items-center gap-1">
+                                        <Check className="h-4 w-4 text-purple-600" />
+                                        <p className="text-sm">
+                                            Verified Purchase
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col gap-4 lg:pr-16">
+                            <div className="flex gap-0.5 mb-2">
+                                <Star className="h-5 w-5 text-purple-600 fill-purple-600" />
+                                <Star className="h-5 w-5 text-purple-600 fill-purple-600" />
+                                <Star className="h-5 w-5 text-purple-600 fill-purple-600" />
+                                <Star className="h-5 w-5 text-purple-600 fill-purple-600" />
+                                <Star className="h-5 w-5 text-purple-600 fill-purple-600" />
+                            </div>
+
+                            <div className="text-lg leading-8">
+                                <p>
+                                    {'"'}The{' '}
+                                    <span className="px-0.5 bg-slate-700 text-white">
+                                        quality of the pillow is amazing
+                                    </span>
+                                    , and the custom print turned out even{' '}
+                                    <span className="px-0.5 bg-slate-700 text-white">
+                                        better than I expected
+                                    </span>
+                                    . It’s been a few months, and the colors are
+                                    still as vibrant as the day I received it.
+                                    It’s the perfect addition to my living room,
+                                    and everyone asks where I got it from!{'"'}
+                                </p>
+                            </div>
+
+                            <div className="flex gap-4 mt-2 items-center">
+                                <Image
+                                    width={50}
+                                    height={50}
+                                    className="rounded-full h-10 w-10 object-cover ring-slate-100"
+                                    src="/users/user-4.jpg"
+                                    alt="User"
+                                />
+                                <div>
+                                    <p className="font-semibold">John</p>
+                                    <div className="flex items-center gap-1">
+                                        <Check className="h-4 w-4 text-purple-600" />
+                                        <p className="text-sm">
+                                            Verified Purchase
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </Container>
